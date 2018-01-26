2eb27c5866fdfe3ea04a34304ad526b07cb602a1
J Raul Vazquez <jraulvc@gmail.com>
2018-01-26 16:47:02 +0200

    minor changes

--- remote-sensing-data/clusters.py
+++ remote-sensing-data/clusters.py
@@ -1,15 +1,27 @@
 # -*- coding: UTF-8 -*-
+'''
+Script to visualzie the different pixel clusterization for different cut-off values.
+
+The municipalities to search for are the ones in mpos
+they are encoded in 5 digits, formated as: 'FFMMM', where
+    FF is for the Federal State (from 01 to 32)
+    MMM is for the municipality (from 001 to X, X depends on each State)
+    
+'''
 import pandas as pd
 import numpy as np
 import glob
 import matplotlib.pyplot as plt
 import os
+##############
+os.chdir("C:\\Users\\Raul Vazquez\\Desktop\\NNproj\\remote-sensing\\remote-sensing-data")
+##############
 
 
 mpos = ['01001','01002','01003'] #list of municipalities to search for
 
 # read all the pickles
-path = './remote-sensing-data/clusters/*.p'
+path = './clusters/*.p'
 pickles = glob.glob(path)
 
 # find the pickles we want (the ones on mpos)
@@ -26,7 +38,7 @@ for i in range(0,len(cls)):
 #plt.figure()
 #plt.imshow(im)
 
-nrows = int(len(cls)/5)
+nrows = int(len(cls)/5) #  tengo 5 cut-off values (de clusterización) para cada municipio
 
 for t in range(0,len(cls)):
         image = im[t]
